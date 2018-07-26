72e30e2b66e2d2ee0de74ebf6a1874520a52d7bc
zack diaz <diaz_zachary@ymail.com>
2018-07-26 10:11:38 -0500

    TestServletWithLogs

--- backend/y_schedule/src/main/java/servlet/testservlet.java
+++ backend/y_schedule/src/main/java/servlet/testservlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.json.JSONObject;
 
 import daos.User_TableBeansImpl;
@@ -19,18 +20,20 @@ import util.JSONHelper;
 public class testservlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public testservlet() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor.
+	 */
+	public testservlet() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		final Logger logger = Logger.getLogger(testservlet.class);
 		User_TableBeansImpl implbean = new User_TableBeansImpl();
 		HttpSession session = request.getSession();
 		JSONObject temp = JSONHelper.parseRequest(request.getReader());
@@ -39,22 +42,27 @@ public class testservlet extends HttpServlet {
 		String password = null;
 		String action = null;
 		username = temp.getString("username");
-		password = temp.getString("password"); 
+		password = temp.getString("password");
 		action = temp.getString("action");
-		
-		switch(action) {
-		case "login": 	JSONHelper.sendResponse(response, implbean.getSecLvlByUsernameAndPassword(username, password));
-						break;
-						
+
+		switch (action) {
+		case "login":
+			JSONHelper.sendResponse(response, implbean.getSecLvlByUsernameAndPassword(username, password));
+			logger.info("Loging is Success");
+			break;
+
 		case "logout":
+			logger.info("Loging is Failure");
 
 		}
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
