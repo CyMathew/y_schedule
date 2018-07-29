8a2b9f1a95ca825477454be97ef8651b6ab4a294
ausmo42 <ausmo42@gmail.com>
2018-07-29 17:03:20 -0500

    created dateTime util on front/backend

---
+++ backend/y_schedule/src/main/java/util/DateTimeHelper.java
@@ -0,0 +1,43 @@
+package util;
+
+import java.sql.Timestamp;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+public class DateTimeHelper {
+
+	public static Timestamp getWeekStart(Timestamp ldt) {
+		
+		return getWeekStart(ldt.toLocalDateTime());
+	}
+	
+public static Timestamp getWeekStart(LocalDateTime ldt) {
+		
+		LocalDateTime temp = ldt;
+		
+		while(temp.getDayOfWeek() != DayOfWeek.SUNDAY)
+			temp = temp.minusDays(1);
+		
+		return Timestamp.valueOf(temp);
+	}
+	
+	public static Timestamp getWeekStartByOffset(int offset) {
+		LocalDateTime temp = LocalDateTime.now();
+		temp.minusDays(7*offset);
+		
+		return getWeekStart(temp);
+	}
+	
+	public static String getDayOfWeekName(int day) {
+		switch(day) {
+		case 0: return "sunday";
+		case 1: return "monday";
+		case 2: return "tuesday";
+		case 3: return "wednesday";
+		case 4: return "thrusday";
+		case 5: return "friday";
+		default: return "saturday";
+		}
+	}
+	
+}
