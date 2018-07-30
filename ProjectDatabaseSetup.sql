2067a3864d6550ab06fad663b6b65793c8ed7e9e
zack diaz <diaz_zachary@ymail.com>
2018-07-30 09:04:24 -0500

    MockUsers

---
+++ ProjectDatabaseSetup.sql
@@ -0,0 +1,119 @@
+﻿SET DEFINE OFF;
+CREATE TABLE USER_TABLE (
+  USER_ID NUMBER(10),
+  USER_FNAME varchar(20) NOT NULL,
+  USER_LNAME varchar(23) NOT NULL,
+  USER_USERNAME varchar(25) UNIQUE,
+  USER_PASSWORD varchar(25) NOT NULL
+  SEC_LVL NUMBER(10),
+  STORE_ID NUMBER(10)
+);
+
+insert into USER_TABLE values(1,'Bell','Pinkston','Swuzzy','Eecahc4iezae',3,101);                         
+insert into USER_TABLE values(2,'Milton','Vibbert','Posuraid68','ooP1aed3fah'2,2367);
+insert into USER_TABLE values(3,'Elisabeth','Miller','Dereement','Ein1onoohee',1,2367);
+insert into USER_TABLE values(4,'Kevin','Cochran','Milisdowered','ood6Ohgai1oo',1,2367);
+insert into USER_TABLE values(5,'Everett','Moffatt','Thentrue','Veequ8shi',1,2367);
+insert into USER_TABLE values(6,'Judith','Adams','Oferds','JiseeVa4ie',1,2367);
+insert into USER_TABLE values(7,'Elizabeth','Nielsen','Sartury','queeLah5J',1,2367);
+insert into USER_TABLE values(8,'Nancy','Riordan','Fromse1961','EiWoht0Itie',1,2367);
+insert into USER_TABLE values(9,'Margaret','Dingman','Andid1947','de4Shayieroo',1,2367);
+insert into USER_TABLE values(10,'Richard','Parsons','Raidearan','neu9eef8P',1,2367);
+
+
+
+
+
+
+
+
+insert into USER_TABLE values('Stephanie','Corrales','Randiall1992','mooSh7hei');
+insert into USER_TABLE values('Dorothy','Henry','Seaut1938','Eeluup9shae');
+insert into USER_TABLE values('Roxana','Hopper','Cacked','phie0Bup9');
+insert into USER_TABLE values('Andrew','Scheerer','Bearted','baiDae3ai');
+insert into USER_TABLE values('Steve','Wood','Ovents','Ieyih6Peit');
+insert into USER_TABLE values('David','Harper','Worgen','ohh7Lie9hei');
+insert into USER_TABLE values('Teresa','Anderson','Wonsize','Lohjohc3ah');
+insert into USER_TABLE values('Salvatore','Williams','Qualmst','ga5Aihei');
+insert into USER_TABLE values('Maryann','Harman','Posinion','Vis6Hohsoh');
+insert into USER_TABLE values('Chester','Small','Faidelper','taTh6Eed');
+insert into USER_TABLE values('Evelyn','Barnes','Resend','aC1aishohPh');
+insert into USER_TABLE values('Donald','Freeman','Goinkill','tie2Eethuoch');
+insert into USER_TABLE values('Mildred','Merz','Witin1984','Aikey1aemai');
+insert into USER_TABLE values('William','Tyson','Cupped1933','rieDae8b');
+insert into USER_TABLE values('Earl','Smith','Seck1984','Da7phi7phei');
+insert into USER_TABLE values('Andrew','Strong','Crocke','Chey0zai2ahtae');
+insert into USER_TABLE values('Robert','Acosta','Ledits','phooc1ooV7u');
+insert into USER_TABLE values('Delores','Bannon','Distaild','maiTiM4yoodee');
+insert into USER_TABLE values('Isidro','Chiesa','Thistalre','DaiDe5ae');
+insert into USER_TABLE values('Ralph','Curtis','Affir1989','uup0Eequ');
+insert into USER_TABLE values('Latosha','Merritt','Carolve','Oosieyi7');
+insert into USER_TABLE values('Janet','Quiroz','Somblifir','si1Shis2ai');
+insert into USER_TABLE values('Cristina','Richardson','Goned1939','ahzie9vaiSh');
+insert into USER_TABLE values('Robert','Monger','Handentoich','ooT2gaen');
+insert into USER_TABLE values('Clarence','Spigner','Ower1943','tohzae1Auy');
+insert into USER_TABLE values('Miguel','Huhn','Advalogiand','De7eez9Leng');
+insert into USER_TABLE values('Kevin','Castillo','Themprod70','ov9oPhoosai');
+insert into USER_TABLE values('George','Wardlaw','Excirs','gooR6Phee');
+insert into USER_TABLE values('Nellie','Perry','Enewalifirs','Vee5ahquee');
+insert into USER_TABLE values('Reynaldo','Craven','Bobjespat62','kuKepe0Ai');
+insert into USER_TABLE values('Dirk','Landgraf','Sinexpond','agieH5the');
+insert into USER_TABLE values('John','Bevins','Hinger33','Ahtoo8aifai');
+insert into USER_TABLE values('Barbara','Smith','Wassitte','Hoh8hee1oh');
+insert into USER_TABLE values('Kenya','Fields','Anting','ohJ8aip2Phe');
+insert into USER_TABLE values('Gary','Horace','Queed1977','Loo1wa3dee');
+insert into USER_TABLE values('Kimberly','Clanton','Fulevess','yee1Ohcee9ie');
+insert into USER_TABLE values('Delores','Thompson','Ingend','Quagiey2mang');
+insert into USER_TABLE values('Omar','Lewis','Clun1948','To9oonoo7ah');
+insert into USER_TABLE values('Jonathan','Epling','Steris61','leiC7ikeuy7');
+insert into USER_TABLE values('Marcus','Charleston','Corant1977','ruHoch0shoo');
+insert into USER_TABLE values('Charles','Zarate','Froopped','Mach4eiD7');
+insert into USER_TABLE values('Stephen','Yager','Disme1935','Xu2ieteegho');
+insert into USER_TABLE values('Harris','Pearson','Tritionver','Ik1jegaegh');
+insert into USER_TABLE values('Sophie','Wright','Themnioncy','Ohqueit7oe');
+insert into USER_TABLE values('Tiffany','Wilson','Prinag','XeeC8wohmie');
+insert into USER_TABLE values('Larry','Glover','Anclow1983','ichah9ODe4e');
+insert into USER_TABLE values('Cheryl','Gentry','Thisch1975','caihei7Ba');
+insert into USER_TABLE values('Betty','Brown','Degiverthe','Hahy7ohqu');
+insert into USER_TABLE values('Dorothy','Howell','Pasters1953','ohy8Hii4oh');
+insert into USER_TABLE values('April','Peterson','Yesett','iewaeZoo1');
+insert into USER_TABLE values('Lee','Brown','Herl1953','ahw6ooXee');
+insert into USER_TABLE values('Brandon','Whetzel','Freples','Quieshoo4oo');
+insert into USER_TABLE values('Kathleen','Clement','Vaunparly','Chi8iiloh8oo');
+insert into USER_TABLE values('Lorraine','Thomas','Beary1953','OoShe5hei8');
+insert into USER_TABLE values('Lisa','Hardin','Extrifformy71','Loc1Iewai');
+insert into USER_TABLE values('Raul','Elliot','Alaim1950','vaY0shahm');
+insert into USER_TABLE values('Jennifer','Beatty','Canythould','miek4Gudah');
+insert into USER_TABLE values('Annie','Russell','Worence','ea8Zigi7e');
+insert into USER_TABLE values('Jennifer','Sweeten','Sexagnat','Ohc8re3ie');
+insert into USER_TABLE values('Kerry','Patel','Affir1971','eithu3aeH8hoh');
+insert into USER_TABLE values('Joan','Hatch','Thavitage94','aim6pahThia8');
+insert into USER_TABLE values('Kim','Lockhart','Hancy1955','shotho6ohCh');
+insert into USER_TABLE values('Anna','Tamplin','Expre1944','EeC0aiwae6v');
+insert into USER_TABLE values('Curtis','Wood','Durn1974','oow9vee0O');
+insert into USER_TABLE values('Jeffrey','Bennett','Samot1946','WaiLeaSho5');
+insert into USER_TABLE values('Michelle','King','Beglinver','queaj4Roo');
+insert into USER_TABLE values('Bryan','Woods','Foustion','Quae1xaoL0');
+insert into USER_TABLE values('Jacob','Callahan','Cier1942','zoofohCh7');
+insert into USER_TABLE values('Alene','Wynn','Overave','ooquoo5Rae');
+insert into USER_TABLE values('Hilda','Garrett','Flach1935','oung9Kie');
+insert into USER_TABLE values('Elena','Gray','Fild1933','di8Athuz');
+insert into USER_TABLE values('Kaci','Lewis','Tomettle','ooGaevee4n');
+insert into USER_TABLE values('Walter','Canales','Thught','Ai5ahgiet6n');
+insert into USER_TABLE values('Betty','Wells','Inseatifee','Ree5iicahthoh');
+insert into USER_TABLE values('Monty','Ring','Larmen82','oos2Yoh8ahx');
+insert into USER_TABLE values('Bradley','Hawkins','Pleatcherge','Aichai2yoo');
+insert into USER_TABLE values('Dexter','Todd','Plaustand43','oowaL2aiph');
+insert into USER_TABLE values('Jerome','Anderson','Brild1967','lohfah9U');
+insert into USER_TABLE values('David','Colson','Praces','yoh3Nu8ue');
+insert into USER_TABLE values('Allen','Butler','Kingince','eex5Ahthazae');
+insert into USER_TABLE values('David','Tuggle','Decough1978','uan7eBie7iu8');
+insert into USER_TABLE values('Jerry','Ashby','Sorinut','yae1aiyeiH');
+insert into USER_TABLE values('Rebecca','Watkins','Leativuld','thoo9Oy8');
+insert into USER_TABLE values('Ruth','Reyer','Themandiones','boj2Quoh4ai');
+insert into USER_TABLE values('Dorothy','Steele','Swen1991','cae2Ahjae8');
+insert into USER_TABLE values('Janette','Bruni','Alem1987','Dei7raeFai');
+insert into USER_TABLE values('Jose','Ford','Showle','hohjai9Ei');
+insert into USER_TABLE values('Flora','Anderson','Daysim','Phaig0aePi');
+insert into USER_TABLE values('Lesley','Aguilar','Andshoughter','feDie5AaD2');
+insert into USER_TABLE values('Barbara','Jackson','Susprold','Aimoo0roop');
