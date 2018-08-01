3f5d4941b1f9632778f3a837db1a437fae026603
zack diaz <diaz_zachary@ymail.com>
2018-08-01 16:27:56 -0500

    selectEmpInfoTest

--- backend/y_schedule/src/test/java/com/revature/test/MangerServiceTest.java
+++ backend/y_schedule/src/test/java/com/revature/test/MangerServiceTest.java
@@ -1,26 +1,29 @@
 package com.revature.test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import beans.UserBean;
+import daos.ManagerDao;
 import daos.UserDao;
 import services.ManagerService;
-import services.UserService;
 
 public class MangerServiceTest {
 	
 	JSONObject tempo = new JSONObject();
+	UserBean bean = new UserBean();
 	ManagerService ms = new ManagerService();
-	
+	ManagerDao md = new ManagerDao();
+	UserDao ud = new UserDao();
 	@Before
 	public void setUp() throws Exception {
-
+		
 	}
 
 	@After
@@ -28,8 +31,19 @@ public class MangerServiceTest {
 	}
 
 	@Test
-	public void test() {
-		assertNotNull(ms.selectEmpInfo(10));
+	public void selectEmpInfoPass() {
+		JSONAssert.assertEquals("{\"userfname\":\"Richard\",\"seclvl\":\"employee\",\"storeId\":2367,\"userid\":10,\"userlname\":\"Parsons\"}", 
+				ms.selectEmpInfo(10), JSONCompareMode.LENIENT);
+		JSONAssert.assertEquals("{\"userfname\":\"manager\",\"seclvl\":\"manager\",\"storeId\":2367,\"userid\":13,\"userlname\":\"manager\"}", 
+				ms.selectEmpInfo(13), JSONCompareMode.LENIENT);
 	}
+	@Test
+	public void selectEmpInforFail() {
+		JSONAssert.assertNotEquals("{\"userfname\":\"Richard\",\"seclvl\":\"employee\",\"storeId\":2367,\"userid\":10,\"userlname\":\"Parsons\"}", 
+				ms.selectEmpInfo(0), JSONCompareMode.LENIENT);
+		JSONAssert.assertNotEquals("{\"userfname\":\"manager\",\"seclvl\":\"manager\",\"storeId\":2367,\"userid\":13,\"userlname\":\"manager\"}", 
+				ms.selectEmpInfo(10), JSONCompareMode.LENIENT);
+	}
+	
 
 }
