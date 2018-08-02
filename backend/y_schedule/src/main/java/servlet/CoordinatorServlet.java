d853a029f7fa3f7c179dcedb4ad777b48608eed1
wang3224 <wang3224@umn.edu>
2018-08-02 18:20:32 -0500

    added a servlet for coordinator

---
+++ backend/y_schedule/src/main/java/servlet/CoordinatorServlet.java
@@ -0,0 +1,40 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import services.CoordinatorService;
+import util.JSONHelper;
+import util.SessionUtil;
+
+
+public class CoordinatorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public CoordinatorServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		CoordinatorService coordinatorservice = new CoordinatorService();
+		JSONObject jsonrequest = SessionUtil.getOriginalRequest(request);
+		JSONHelper.sendResponse(response, coordinatorservice.gateKeeper(jsonrequest));
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
