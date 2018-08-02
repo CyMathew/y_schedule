80b72b0dbd3f11b93614a8dcf23f8ea60185989f
ausmo42 <ausmo42@gmail.com>
2018-08-02 17:56:18 -0500

    messages are colored correctly

--- frontend/y-schedule/src/app/directives/message-color.directive.ts
+++ frontend/y-schedule/src/app/directives/message-color.directive.ts
@@ -1,16 +1,32 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appMessageColor]'
 })
 export class MessageColorDirective {
 
-  @Input('owner') owner: boolean;
+  @Input() appMessageColor: boolean;
+  protected _elementClass: string[] = [];
 
-  constructor(private el: ElementRef) { 
-    if(this.owner)
-    el.nativeElement.class = ""
+  constructor(private el: ElementRef) {
+    //el.nativeElement.style.backgroundColor = 'yellow';
 
   }
 
+  @HostBinding('class')
+  get elementClass(): string {
+    return this._elementClass.join(' ');
+  }
+
+  @HostListener('load') onMouseEnter() {
+
+  }
+
+  ngOnInit() {
+    console.log("onload", this.appMessageColor);
+    if (this.appMessageColor)
+      this._elementClass.push("message-section message-self");
+    else
+      this._elementClass.push("message-section message-other");
+  }
 }
